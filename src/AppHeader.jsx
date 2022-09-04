18eb474c3446fefb87200e20545c046ea0d09f11
Qori El-Hafizh <contact@qori.dev>
2022-09-05 02:57:17 +0700

    darkmode positioning

--- src/AppHeader.jsx
+++ src/AppHeader.jsx
@@ -5,27 +5,25 @@ import { DarkModeContext } from "./DarkModeContext";
 export default function AppHeader() {
     const { darkMode, setDarkMode } = useContext(DarkModeContext);
     return (
-        <header className='container'>
+        <header className='header container' style={{ marginBottom: "1.5rem" }}>
             <div>
-                <div style={{ marginBottom: "1.5rem" }}>
 
-                    <h1 className="text-xl">Social Media Dashboard</h1>
-                    <p className="text-bold">
-                        Total Followers: 23,004
-                    </p>
+                <h1 className="text-xl">Social Media Dashboard</h1>
+                <p className="text-bold">
+                    Total Followers: 23,004
+                </p>
+            </div>
+            <div className="darkmode-bar">
+                <div className="text-bold">
+                    Dark Mode
                 </div>
-                <div className="darkmode-bar">
-                    <div className="text-bold">
-                        Dark Mode
-                    </div>
-                    <div className="darkmode-switch"
-                        onClick={e => {
-                            setDarkMode(!darkMode);
-                            console.log(darkMode);
-                        }}
-                    >
-                        <div data-value={darkMode} className="darkmode-slider">
-                        </div>
+                <div className="darkmode-switch"
+                    onClick={e => {
+                        setDarkMode(!darkMode);
+                        console.log(darkMode);
+                    }}
+                >
+                    <div data-value={darkMode} className="darkmode-slider">
                     </div>
                 </div>
             </div>
